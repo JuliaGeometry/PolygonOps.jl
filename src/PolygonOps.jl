c26edc0f11cd47ffa59e988b7cbef23935740f52
steve <kd2cca@gmail.com>
2019-09-13 22:12:34 -0400

    initial pass at hao algo

--- src/PolygonOps.jl
+++ src/PolygonOps.jl
@@ -1,5 +1,61 @@
 module PolygonOps
 
-greet() = print("Hello World!")
+export isinside
+
+"""
+isinside
+
+Hao algorithm
+https://www.researchgate.net/publication/328261689_Optimal_Reliable_Point-in-Polygon_Test_and_Differential_Coding_Boolean_Operations_on_Polygons
+"""
+function isinside(p, poly)
+    k = 0
+
+    xp = p[1]
+    yp = p[2]
+
+    PT = eltype(p)
+
+    for i = 1:length(poly)-1
+        v1 = poly[i][2] - yp
+        v2 = poly[i+1][2] - yp
+
+        if v1 < zero(PT) && v2 < zero(PT) || v1 > zero(PT) && v2 > zero(PT)
+            continue
+        end
+
+        u1 = poly[i][1] - xp
+        u2 = poly[i+1][1] - xp
+
+        f = (u1 * v2) - (u2 * v1)
+
+        if v2 > zero(PT) && v1 <= zero(PT)
+            if f > zero(PT)
+                k += 1
+            elseif iszero(f)
+                return -1
+            end
+        elseif v1 > zero(PT) && v2 <= zero(PT)
+            if f < zero(PT)
+                k += 1
+            elseif iszero(f)
+                return -1
+            end
+        elseif iszero(v2) && v1 < zero(PT)
+            iszero(f) && return -1
+        elseif iszero(v1) && v2 < zero(PT)
+            iszero(f) && return -1
+        elseif iszero(v1) && iszero(v2)
+            if u2 <= zero(PT) && u1 >= zero(PT)
+                return -1
+            elseif u1 <= zero(PT) && u2 >= zero(PT)
+                return -1
+            end
+        end
+    end
+
+    iszero(k % 2) && return 0
+    return 1
+end
 
 end # module
