dccaa7a487ce2989e7f4f84d050dec7277395eba
Steve Kelly <kd2cca@gmail.com>
2020-01-12 22:40:59 -0500

    add additional test for reversed

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,10 +6,12 @@ edge(p1, p2) = (SVector(p1),SVector(p2))
 
 @testset "inpolygon" begin
     poly1 = SVector{2,Int}[(0,0),(0,10),(10,10),(10,0),(0,0)]
-    @test inpolygon(SVector(5,5),poly1) == 1 #in
-    @test inpolygon(SVector(-5,-5),poly1) == 0 #out
-    @test inpolygon(SVector(0,5),poly1) == -1 #on
-    @test inpolygon(SVector(5,10),poly1) == -1 #on
-    @test inpolygon(SVector(10,10),poly1) == -1 #on
-    @test inpolygon(SVector(0,0),poly1) == -1 #on
+    for poly in (poly1, reverse(poly1))
+        @test inpolygon(SVector(5,5),poly1) == 1 #in
+        @test inpolygon(SVector(-5,-5),poly1) == 0 #out
+        @test inpolygon(SVector(0,5),poly1) == -1 #on
+        @test inpolygon(SVector(5,10),poly1) == -1 #on
+        @test inpolygon(SVector(10,10),poly1) == -1 #on
+        @test inpolygon(SVector(0,0),poly1) == -1 #on
+    end
 end
